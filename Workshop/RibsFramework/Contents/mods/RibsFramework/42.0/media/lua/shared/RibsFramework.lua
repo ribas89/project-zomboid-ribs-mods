3ec83bd190eb332622bf5d2c1f3d887f8c4c037c
ribas89 <ribas89@proton.me>
2025-08-30 04:26:30 -0300

    Added Ingame Intervals

--- Workshop/RibsFramework/Contents/mods/RibsFramework/42.0/media/lua/shared/RibsFramework.lua
+++ Workshop/RibsFramework/Contents/mods/RibsFramework/42.0/media/lua/shared/RibsFramework.lua
@@ -4,6 +4,10 @@ RibsFramework.Interval={}
 RibsFramework.Interval.__index = RibsFramework.Interval
 require("RibsFrameworkInterval")
 
+RibsFramework.IntervalIngame={}
+RibsFramework.IntervalIngame.__index = RibsFramework.IntervalIngame
+require("RibsFrameworkIntervalIngame")
+
 RibsFramework.Sandbox = {}
 RibsFramework.Sandbox.__index = RibsFramework.Sandbox
 require("RibsFrameworkSandbox")
---
+++ Workshop/RibsFramework/Contents/mods/RibsFramework/42.0/media/lua/shared/RibsFrameworkIntervalIngame.lua
@@ -0,0 +1,66 @@
+function RibsFramework.IntervalIngame:new(args)
+    args = args or {}
+
+    local instance = setmetatable({}, self)
+
+    instance.interval = args.interval or 1
+    instance.start = args.start or 0
+
+    instance.events = args.events or {
+        EveryOneMinute = Events.EveryOneMinute,
+        EveryTenMinutes = Events.EveryTenMinutes,
+        EveryDays = Events.EveryDays,
+        EveryHours = Events.EveryHours,
+        OnDusk = Events.OnDusk,
+        OnDawn = Events.OnDawn
+    }
+
+    instance.eventType = args.eventType or "EveryOneMinute"
+
+    for eventType, _ in pairs(instance.events) do
+        instance:argsMultiplier(args, eventType)
+    end
+
+    instance.triggerCount = instance.start
+
+    instance.handlers = args.handlers or {}
+
+    instance.events[instance.eventType].Add(function()
+        instance:onTrigger()
+    end)
+
+    return instance
+end
+
+function RibsFramework.IntervalIngame:argsMultiplier(args, eventType)
+    if not args[eventType] then return end
+
+    self.interval = args[eventType]
+    self.eventType = eventType
+end
+
+
+function RibsFramework.IntervalIngame:onTrigger()
+    self.triggerCount = self.triggerCount + 1
+
+    if not self.handlers then return end
+
+    if not self:itsTime() then return end
+
+    for _, handler in ipairs(self.handlers) do
+        handler()
+    end
+end
+
+function RibsFramework.IntervalIngame:itsTime()
+    local interval = (type(self.interval) == "function" and self.interval(self)) or self.interval
+
+    if self.triggerCount < interval then return false end
+
+    self.triggerCount = 0
+    return true
+end
+
+function RibsFramework.IntervalIngame:add(handler)
+    table.insert(self.handlers, handler)
+end
