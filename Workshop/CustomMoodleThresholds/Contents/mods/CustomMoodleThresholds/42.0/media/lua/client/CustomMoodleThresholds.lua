18d5ad1eec0dd9ac9be628554fc837fa034b73a4
ribas89 <ribas89@proton.me>
2025-09-02 14:54:28 -0300

    add new mod custom moodle thresholds

---
+++ Workshop/CustomMoodleThresholds/Contents/mods/CustomMoodleThresholds/42.0/media/lua/client/CustomMoodleThresholds.lua
@@ -0,0 +1,5 @@
+require("RibsFramework")
+
+CustomMoodleThresholds = CustomMoodleThresholds or {}
+
+CustomMoodleThresholds.sandbox = RibsFramework.Sandbox:new({ ID = "CustomMoodleThresholds", autoModOptions = true })
