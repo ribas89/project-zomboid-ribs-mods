06321441e7e7d2994ccda59c6523edf062313178
ribas89 <ribas89@proton.me>
2025-09-05 16:35:09 -0300

    add new mod custom zoom parameter

---
+++ Workshop/CustomZoomParameter/Contents/mods/CustomZoomParameter/42.0/media/lua/client/CustomZoomParameter.lua
@@ -0,0 +1,3 @@
+local CustomZoomParameter = PZAPI.ModOptions:create("CustomZoomParameter", "Custom Zoom Parameter")
+CustomZoomParameter:addTextEntry("stringzoom", "Zooms",
+	"12.0;9.0;6.0;5.0;4.0;3.0;2.5;2.25;2.0;1.75;1.5;1.25;1.0;0.75;0.5;0.25;0.15;0.10;0.05;0.025;0.020;0.015;0.01;0.005;0.004;0.003;0.002;0.001")
