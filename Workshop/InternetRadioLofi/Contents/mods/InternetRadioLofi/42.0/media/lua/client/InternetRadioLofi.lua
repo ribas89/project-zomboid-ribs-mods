1e372948bb65bceb1d0472b2440479460f53303a
ribas89 <ribas89@proton.me>
2026-01-26 18:32:35 -0300

    new mod: Internet Radio - Lofi

---
+++ Workshop/InternetRadioLofi/Contents/mods/InternetRadioLofi/42.0/media/lua/client/InternetRadioLofi.lua
@@ -0,0 +1,46 @@
+InternetRadio:addStation(250.0, "Best Synth 2024", "https://lofi.ydns.eu/bestsynth2024.mp3")
+InternetRadio:addStation(250.2, "Lofi Autumn", "https://lofi.ydns.eu/lofiautumn.mp3")
+InternetRadio:addStation(250.4, "Lofi Lego", "https://lofi.ydns.eu/lofilego.mp3")
+InternetRadio:addStation(250.6, "Lofi Summer", "https://lofi.ydns.eu/lofisummer.mp3")
+
+InternetRadio:addStation(250.8, "Chill Guitar", "https://lofi.ydns.eu/chillguitar.mp3")
+InternetRadio:addStation(251.0, "Lofi Brazilian", "https://lofi.ydns.eu/lofibrazilian.mp3")
+InternetRadio:addStation(251.2, "Lofi Medieval", "https://lofi.ydns.eu/lofimedieval.mp3")
+InternetRadio:addStation(251.4, "Lofi Synth", "https://lofi.ydns.eu/lofisynth.mp3")
+
+InternetRadio:addStation(251.6, "Chill House", "https://lofi.ydns.eu/chillhouse.mp3")
+InternetRadio:addStation(251.8, "Lofi Chess", "https://lofi.ydns.eu/lofichess.mp3")
+InternetRadio:addStation(252.0, "Lofi Middle East", "https://lofi.ydns.eu/lofimiddleeast.mp3")
+InternetRadio:addStation(252.2, "Peaceful Piano", "https://lofi.ydns.eu/peacefulpiano.mp3")
+
+InternetRadio:addStation(252.4, "Christmas Music", "https://lofi.ydns.eu/christmasmusic.mp3")
+InternetRadio:addStation(252.6, "Lofi Christmas", "https://lofi.ydns.eu/lofichristmas.mp3")
+InternetRadio:addStation(252.8, "Lofi Pewdz Marzia", "https://lofi.ydns.eu/lofipewdzmarzia.mp3")
+InternetRadio:addStation(253.0, "Relaxing Jazz", "https://lofi.ydns.eu/relaxingjazz.mp3")
+
+InternetRadio:addStation(253.2, "Dark Ambient", "https://lofi.ydns.eu/darkambient.mp3")
+InternetRadio:addStation(253.4, "Lofi Classical", "https://lofi.ydns.eu/loficlassical.mp3")
+InternetRadio:addStation(253.6, "Lofi Reading", "https://lofi.ydns.eu/lofireading.mp3")
+InternetRadio:addStation(253.8, "Sleep Ambient", "https://lofi.ydns.eu/sleepambient.mp3")
+
+InternetRadio:addStation(254.0, "Gaming Music", "https://lofi.ydns.eu/gamingmusic.mp3")
+InternetRadio:addStation(254.2, "Lofi French", "https://lofi.ydns.eu/lofifrench.mp3")
+InternetRadio:addStation(254.4, "Lofi Sad", "https://lofi.ydns.eu/lofisad.mp3")
+InternetRadio:addStation(254.6, "Study Session", "https://lofi.ydns.eu/studysession.mp3")
+
+InternetRadio:addStation(254.8, "Halloween Lofi", "https://lofi.ydns.eu/halloweenlofi.mp3")
+InternetRadio:addStation(255.0, "Lofi Gentle Chill", "https://lofi.ydns.eu/lofigentlechill.mp3")
+InternetRadio:addStation(255.2, "Lofi Shadows", "https://lofi.ydns.eu/lofishadows.mp3")
+InternetRadio:addStation(255.4, "Synth Ambient", "https://lofi.ydns.eu/synthambient.mp3")
+
+InternetRadio:addStation(255.6, "Halloween Music", "https://lofi.ydns.eu/halloweenmusic.mp3")
+InternetRadio:addStation(255.8, "Lofi Gentle Synth", "https://lofi.ydns.eu/lofigentlesynth.mp3")
+InternetRadio:addStation(256.0, "Lofi Sleep", "https://lofi.ydns.eu/lofisleep.mp3")
+
+InternetRadio:addStation(256.2, "Lazy Vibes", "https://lofi.ydns.eu/lazyvibes.mp3")
+InternetRadio:addStation(256.4, "Lofi Hip Hop", "https://lofi.ydns.eu/lofihiphop.mp3")
+InternetRadio:addStation(256.6, "Lofi Study 2023", "https://lofi.ydns.eu/lofistudy2023.mp3")
+
+InternetRadio:addStation(256.8, "Lofi Asian", "https://lofi.ydns.eu/lofiasian.mp3")
+InternetRadio:addStation(257.0, "Lofi Jazz", "https://lofi.ydns.eu/lofijazz.mp3")
+InternetRadio:addStation(257.2, "Lofi Study 2025", "https://lofi.ydns.eu/lofistudy2025.mp3")
